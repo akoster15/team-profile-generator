efe650b7da090a9c62eeaaba9386c61d42860e91
Andrew Koster <andrewkoster95@gmail.com>
2021-07-13 21:21:39 -0400

    Imported classes to index.js and created the file htmlGenerator.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//Node modules
+//Import node modules
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
+
+//Import classes
+const Engineer = require("./lib/Engineer.test");
+const Intern = require("./lib/Intern.test");
+const Manager = require("./lib/Manager.test");
+const htmlGenerator = require("./src/htmlGenerator");
+const members = [];
