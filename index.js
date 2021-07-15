a9364ae5b97d8dda92b879dfb8b5cc1cd9de13ca
Andrew Koster <andrewkoster95@gmail.com>
2021-07-14 23:50:33 -0400

    code cleanup

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const htmlGenerator = require("./src/htmlGenerator");
-const membersArray = [];
+let employeesArr = [];
 
 
 //Questions array for all employees
