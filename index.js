79bc72446e4191b50c42686ae3c948d5be73a8c8
Andrew Koster <andrewkoster95@gmail.com>
2021-07-15 22:00:26 -0400

    Added all the functions for questions to index.js

--- index.js
+++ index.js
@@ -9,5 +9,139 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const htmlGenerator = require("./src/htmlGenerator");
-const employees = [];
+var employees = [];
 
+//User chooses type of employee
+function employeeType() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What is the role of this employee?",
+            choices: ["Engineer", "Manager", "Intern", "Finished"]
+        }
+    ]) . then((responses) => {
+        switch (responses.role) {
+            case "Engineer":
+                addEngineer()
+                break;
+            case "Manager":
+                addManager()
+                break;
+            case "Intern":
+                addIntern()
+            case "Finished":
+                finishApp()
+            default:
+                break;
+        }
+    })
+}
+
+//Questions for user if they selected "Engineer"
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's ID number?",
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's Github username?",
+        },
+    ])
+    .then((responses) => {
+        var engineer = new Engineer(
+            responses.engineerName,
+            responses.engineerId,
+            responses.engineerEmail,
+            responses.engineerGithub
+        );
+        employees.push(engineer)
+        employeeType()
+    });
+}
+
+//Questions for user if they selected "Manager"
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the manager's ID number?",
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's email address?",
+        },
+        {
+            type: "input",
+            name: "managerOfficeNum",
+            message: "What is the manager's office number?",
+        },
+    ])
+    .then((responses) => {
+        var manager = new Manager(
+            responses.managerName,
+            responses.managerId,
+            responses.managerEmail,
+            responses.managerOfficeNum
+        );
+        employees.push(manager)
+        employeeType()
+    });
+}
+
+//Questions for user if they selected "Intern"
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the intern's ID number?",
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's office number?",
+        },
+    ])
+    .then((responses) => {
+        var manager = new Manager(
+            responses.managerName,
+            responses.managerId,
+            responses.managerEmail,
+            responses.managerOfficeNum
+        );
+        employees.push(manager)
+        employeeType()
+    });
+}
