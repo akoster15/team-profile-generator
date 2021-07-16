f77b6c1ebb15ab0cf83f7f31286c7d28c35298c4
Andrew Koster <andrewkoster95@gmail.com>
2021-07-15 22:39:08 -0400

    added classes to generateHTML.js

---
+++ src/htmlGenerator.js
@@ -0,0 +1,6 @@
+//Import classes
+const index = require('../index');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
+
